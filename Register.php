6397bfb1fb2937738a077a70482eb897ba1adcbc
Chazza007 <crthedevil@gmail.com>
2016-06-19 21:46:36 +0100

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,5 +1,5 @@
 <?php
-    $con = mysqli_connect("31.170.160.100", "a3170397_Chazz", "Chipper007", "a3170397_GeogApp");
+    $con = mysqli_connect("http://geographyapp.net16.net", "a3170397_Chazz", "Chipper007", "a3170397_GeogApp");
     
     $name = $_POST["name"];
     $age = $_POST["email"];
