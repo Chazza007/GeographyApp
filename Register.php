1da904ba63aabbd15da5d03b86fe36aeae6f7d47
Chazza007 <crthedevil@gmail.com>
2016-06-19 21:55:00 +0100

    Update Register.php

--- Register.php
+++ Register.php
@@ -7,7 +7,7 @@
     $password = $_POST["password"];
 
     $statement = mysqli_prepare($con, "INSERT INTO User (name, username, email, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "sssss", $name, $username, $email, $password);
+    mysqli_stmt_bind_param($statement, "ssss", $name, $username, $email, $password);
     mysqli_stmt_execute($statement);
     
     $response = array();
